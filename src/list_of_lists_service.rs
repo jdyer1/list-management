1d592e6051dfdaff6f3d6dde607913a06472d56d
jdyer1 <jdyer@apache.org>
2024-07-22 15:23:51 -0500

    paging

--- src/list_of_lists_service.rs
+++ src/list_of_lists_service.rs
@@ -22,6 +22,15 @@ pub fn retrieve_lists(context: impl LMContext,
                       return_attributes: bool,
                       return_rollups: bool) -> Vec<ListResult> {
     let mut a = context.list_storage().all_lists();
+    let start = paging.start as usize;
+    let mut end = (paging.start + paging.rows) as usize;
+    if end > a.len() {
+        end = a.len();
+    }
+
+    if paging.rows == 0 || paging.start >= a.len() as u64 {
+        return vec![];
+    }
     a.sort_by(|a, b| {
         let (one, two) = if sort.descending { (b, a) } else { (a, b) };
         match sort.key {
@@ -29,12 +38,21 @@ pub fn retrieve_lists(context: impl LMContext,
             SortKey::NAME => one.name.cmp(&two.name),
         }
     });
-
-    a.into_iter()
-        .map(|il| ListResult {
-            list: il,
-            rollups: vec![],
-        }).collect()
+    let mut i:usize = 0;
+    let mut a1:Vec<ListResult> = Vec::new();
+    for item_list in a {
+        if i>=start {
+            a1.push(ListResult {
+                list: item_list,
+                rollups: vec![],
+            });
+        }
+        i = i + 1;
+        if i==end {
+          break;
+        }
+    }
+    a1
 }
 
 #[cfg(test)]
@@ -46,34 +64,65 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_retrieve_all_lists() {
+    fn test_retrieve_all_lists_by_id() {
         let sortReq = sort(SortKey::ID, false);
         let results = retrieve_lists(context(), selector(), paging(0, 10), sortReq, true, true);
         assert_eq!(3, results.len());
         assert_eq!(1, results[0].list.id);
         assert_eq!(2, results[1].list.id);
         assert_eq!(3, results[2].list.id);
+    }
 
+    #[test]
+    fn test_retrieve_all_lists_by_name() {
         let sortReq = sort(SortKey::NAME, false);
         let results = retrieve_lists(context(), selector(), paging(0, 10), sortReq, true, true);
         assert_eq!(3, results.len());
         assert_eq!("A3", results[0].list.name);
         assert_eq!("B1", results[1].list.name);
         assert_eq!("C2", results[2].list.name);
+    }
 
+    #[test]
+    fn test_retrieve_all_lists_by_id_descending() {
         let sortReq = sort(SortKey::ID, true);
         let results = retrieve_lists(context(), selector(), paging(0, 10), sortReq, true, true);
         assert_eq!(3, results.len());
         assert_eq!(3, results[0].list.id);
         assert_eq!(2, results[1].list.id);
         assert_eq!(1, results[2].list.id);
+    }
 
+    #[test]
+    fn test_retrieve_all_lists_by_name_descending() {
         let sortReq = sort(SortKey::NAME, true);
         let results = retrieve_lists(context(), selector(), paging(0, 10), sortReq, true, true);
         assert_eq!(3, results.len());
         assert_eq!("C2", results[0].list.name);
         assert_eq!("B1", results[1].list.name);
         assert_eq!("A3", results[2].list.name);
+    }
+
+    #[test]
+    fn test_retrieve_all_lists_with_paging() {
+        let sortReq = sort(SortKey::ID, false);
+        let results = retrieve_lists(context(), selector(), paging(1, 1), sortReq, true, true);
+        assert_eq!(1, results.len());
+        assert_eq!(2, results[0].list.id);
+    }
+
+    #[test]
+    fn test_retrieve_all_lists_with_paging_beyond_end() {
+        let sortReq = sort(SortKey::ID, false);
+        let results = retrieve_lists(context(), selector(), paging(3, 10), sortReq, true, true);
+        assert_eq!(0, results.len());
+    }
+
+    #[test]
+    fn test_retrieve_all_lists_with_no_rows_requested() {
+        let sortReq = sort(SortKey::ID, false);
+        let results = retrieve_lists(context(), selector(), paging(0, 0), sortReq, true, true);
+        assert_eq!(0, results.len());
     }
 
     fn item_list(id: u64, name: String, folder: String) -> ItemList {
